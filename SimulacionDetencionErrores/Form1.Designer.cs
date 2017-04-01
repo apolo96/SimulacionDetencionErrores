fe18a16730c8d68f460f2c62d89f2f60f66dacd2
apolo96 <andresfpvclap@gmail.com>
2017-04-01 15:52:16 -0500

    Add project files.

---
+++ SimulacionDetencionErrores/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace SimulacionDetencionErrores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metodos = new MetroFramework.Controls.MetroTabControl();
+            this.VCR = new MetroFramework.Controls.MetroTabPage();
+            this.metroTile3 = new MetroFramework.Controls.MetroTile();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.LRC = new MetroFramework.Controls.MetroTabPage();
+            this.CRC = new MetroFramework.Controls.MetroTabPage();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.SC = new MetroFramework.Controls.MetroTabPage();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.metodos.SuspendLayout();
+            this.VCR.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metodos
+            // 
+            this.metodos.Controls.Add(this.VCR);
+            this.metodos.Controls.Add(this.LRC);
+            this.metodos.Controls.Add(this.CRC);
+            this.metodos.Controls.Add(this.SC);
+            this.metodos.Location = new System.Drawing.Point(23, 63);
+            this.metodos.Name = "metodos";
+            this.metodos.SelectedIndex = 0;
+            this.metodos.Size = new System.Drawing.Size(882, 389);
+            this.metodos.Style = MetroFramework.MetroColorStyle.Teal;
+            this.metodos.TabIndex = 0;
+            this.metodos.Tag = "";
+            this.metodos.UseSelectable = true;
+            // 
+            // VCR
+            // 
+            this.VCR.Controls.Add(this.metroLabel6);
+            this.VCR.Controls.Add(this.metroButton1);
+            this.VCR.Controls.Add(this.metroTile3);
+            this.VCR.Controls.Add(this.metroLabel3);
+            this.VCR.Controls.Add(this.metroTile2);
+            this.VCR.Controls.Add(this.metroLabel2);
+            this.VCR.Controls.Add(this.metroTextBox2);
+            this.VCR.Controls.Add(this.metroTile1);
+            this.VCR.Controls.Add(this.metroLabel1);
+            this.VCR.Controls.Add(this.metroTextBox1);
+            this.VCR.HorizontalScrollbarBarColor = true;
+            this.VCR.HorizontalScrollbarHighlightOnWheel = false;
+            this.VCR.HorizontalScrollbarSize = 10;
+            this.VCR.Location = new System.Drawing.Point(4, 38);
+            this.VCR.Name = "VCR";
+            this.VCR.Size = new System.Drawing.Size(874, 347);
+            this.VCR.TabIndex = 0;
+            this.VCR.Text = "Verficacíon Vertical (VRC)";
+            this.VCR.VerticalScrollbarBarColor = true;
+            this.VCR.VerticalScrollbarHighlightOnWheel = false;
+            this.VCR.VerticalScrollbarSize = 10;
+            // 
+            // metroTile3
+            // 
+            this.metroTile3.ActiveControl = null;
+            this.metroTile3.Location = new System.Drawing.Point(603, 95);
+            this.metroTile3.Name = "metroTile3";
+            this.metroTile3.Size = new System.Drawing.Size(239, 175);
+            this.metroTile3.Style = MetroFramework.MetroColorStyle.Teal;
+            this.metroTile3.TabIndex = 12;
+            this.metroTile3.Text = "metroTile3";
+            this.metroTile3.UseSelectable = true;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(603, 64);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(111, 19);
+            this.metroLabel3.TabIndex = 10;
+            this.metroLabel3.Text = "Deteccin de Error";
+            // 
+            // metroTile2
+            // 
+            this.metroTile2.ActiveControl = null;
+            this.metroTile2.Location = new System.Drawing.Point(323, 133);
+            this.metroTile2.Name = "metroTile2";
+            this.metroTile2.Size = new System.Drawing.Size(236, 137);
+            this.metroTile2.Style = MetroFramework.MetroColorStyle.Teal;
+            this.metroTile2.TabIndex = 8;
+            this.metroTile2.Text = "metroTile2";
+            this.metroTile2.UseSelectable = true;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(323, 64);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(90, 19);
+            this.metroLabel2.TabIndex = 7;
+            this.metroLabel2.Text = "Ingresar Error";
+            // 
+            // metroTextBox2
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox2.CustomButton.Image = null;
+            this.metroTextBox2.CustomButton.Location = new System.Drawing.Point(214, 1);
+            this.metroTextBox2.CustomButton.Name = "";
+            this.metroTextBox2.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox2.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox2.CustomButton.TabIndex = 1;
+            this.metroTextBox2.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox2.CustomButton.UseSelectable = true;
+            this.metroTextBox2.CustomButton.Visible = false;
+            this.metroTextBox2.Lines = new string[0];
+            this.metroTextBox2.Location = new System.Drawing.Point(323, 95);
+            this.metroTextBox2.MaxLength = 32767;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PasswordChar = '\0';
+            this.metroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.SelectionLength = 0;
+            this.metroTextBox2.SelectionStart = 0;
+            this.metroTextBox2.ShortcutsEnabled = true;
+            this.metroTextBox2.Size = new System.Drawing.Size(236, 23);
+            this.metroTextBox2.TabIndex = 6;
+            this.metroTextBox2.UseSelectable = true;
+            this.metroTextBox2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(39, 133);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(236, 137);
+            this.metroTile1.Style = MetroFramework.MetroColorStyle.Teal;
+            this.metroTile1.TabIndex = 5;
+            this.metroTile1.Text = "metroTile1";
+            this.metroTile1.UseSelectable = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(39, 288);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(145, 38);
+            this.metroButton1.TabIndex = 4;
+            this.metroButton1.Text = "Aceptar";
+            this.metroButton1.UseSelectable = true;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(39, 64);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(88, 19);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "Ingresar Dato";
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(214, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(39, 95);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(236, 23);
+            this.metroTextBox1.TabIndex = 2;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // LRC
+            // 
+            this.LRC.HorizontalScrollbarBarColor = true;
+            this.LRC.HorizontalScrollbarHighlightOnWheel = false;
+            this.LRC.HorizontalScrollbarSize = 10;
+            this.LRC.Location = new System.Drawing.Point(4, 38);
+            this.LRC.Name = "LRC";
+            this.LRC.Size = new System.Drawing.Size(874, 332);
+            this.LRC.TabIndex = 1;
+            this.LRC.Text = "Verificacíon Longitudinal (LRC)";
+            this.LRC.VerticalScrollbarBarColor = true;
+            this.LRC.VerticalScrollbarHighlightOnWheel = false;
+            this.LRC.VerticalScrollbarSize = 10;
+            // 
+            // CRC
+            // 
+            this.CRC.HorizontalScrollbarBarColor = true;
+            this.CRC.HorizontalScrollbarHighlightOnWheel = false;
+            this.CRC.HorizontalScrollbarSize = 10;
+            this.CRC.Location = new System.Drawing.Point(4, 38);
+            this.CRC.Name = "CRC";
+            this.CRC.Size = new System.Drawing.Size(874, 332);
+            this.CRC.TabIndex = 2;
+            this.CRC.Text = "Verificacíon Ciclica (CRC)";
+            this.CRC.VerticalScrollbarBarColor = true;
+            this.CRC.VerticalScrollbarHighlightOnWheel = false;
+            this.CRC.VerticalScrollbarSize = 10;
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.metroLabel4.Location = new System.Drawing.Point(818, 455);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(95, 15);
+            this.metroLabel4.TabIndex = 1;
+            this.metroLabel4.Text = "Copyright © 2017";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.metroLabel5.Location = new System.Drawing.Point(769, 455);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(50, 15);
+            this.metroLabel5.TabIndex = 2;
+            this.metroLabel5.Text = "Apolo96";
+            // 
+            // SC
+            // 
+            this.SC.HorizontalScrollbarBarColor = true;
+            this.SC.HorizontalScrollbarHighlightOnWheel = false;
+            this.SC.HorizontalScrollbarSize = 10;
+            this.SC.Location = new System.Drawing.Point(4, 38);
+            this.SC.Name = "SC";
+            this.SC.Size = new System.Drawing.Size(874, 332);
+            this.SC.TabIndex = 3;
+            this.SC.Text = "Suma de Comprobacíon";
+            this.SC.VerticalScrollbarBarColor = true;
+            this.SC.VerticalScrollbarHighlightOnWheel = false;
+            this.SC.VerticalScrollbarSize = 10;
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.AutoSize = true;
+            this.metroLabel6.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel6.Location = new System.Drawing.Point(14, 18);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(286, 25);
+            this.metroLabel6.TabIndex = 14;
+            this.metroLabel6.Text = "Verificacion de Redundancia Vertical";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 482);
+            this.Controls.Add(this.metroLabel5);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.metodos);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Teal;
+            this.Text = "Detección de Errores";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metodos.ResumeLayout(false);
+            this.VCR.ResumeLayout(false);
+            this.VCR.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTabControl metodos;
+        private MetroFramework.Controls.MetroTabPage VCR;
+        private MetroFramework.Controls.MetroTabPage LRC;
+        private MetroFramework.Controls.MetroTabPage CRC;
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroTile metroTile2;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroTile metroTile3;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroTabPage SC;
+    }
+}
+
